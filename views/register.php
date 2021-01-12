ca64167a434843f19be617e9c61cab863ddd10e8
ShaikhDanish04 <shaikh.danish4444@gmail.com>
2021-01-12 12:07:29 +0530

    register ui ready

---
+++ views/register.php
@@ -0,0 +1,33 @@
+<div class="container d-flex align-items-center justify-content-center flex-column my-4" style="min-height:90vh">
+    <div class="card mb-4" style="width:300px">
+        <div class="card-body">
+            <p class="h2 text-center font-weight-normal mt-4 mb-5">REGISTER</p>
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="">Email</label>
+                    <input type="text" name="" class="form-control">
+                    <small class="text-muted">*Enter Your Email</small>
+                </div>
+                <div class="form-group">
+                    <label for="">Mobile</label>
+                    <input type="text" name="" class="form-control">
+                    <small class="text-muted">*Enter Your Mobile</small>
+                </div>
+                <div class="form-group">
+                    <label for="">Password</label>
+                    <input type="password" name="" class="form-control">
+                    <small class="text-muted">*Enter Your Password</small>
+                </div>
+                <div class="form-group">
+                    <label for="">Confirm Password</label>
+                    <input type="password" name="" class="form-control">
+                    <small class="text-muted">*Re-enter Your Password</small>
+                </div>
+                <div class="form-group w-100">
+                    <button class="btn btn-success w-100 mb-3">Register</button>
+                    <p class="text-center">Already Registered ? <a href="login">Login</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
