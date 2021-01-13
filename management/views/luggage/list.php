b3ce891377d0a9f9039ffe561c489e723245a049
ShaikhDanish04 <shaikh.danish4444@gmail.com>
2021-01-13 19:35:07 +0530

    RWD test 1

--- management/views/luggage/list.php
+++ management/views/luggage/list.php
@@ -1,60 +1,68 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
-</head>
-
-<body>
+<script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
 
 
-    <video id="preview"></video>
-
-    <div class="cameras">
-        <!-- <button class="btn btn-primary start">Start</button> -->
+<div class="container-fluid">
+    <div class="card" style="width: 350px">
+        <div class="card-body text-center">
+            <p class="h3">QR Code Scanner</p>
+            <div class="rounded mb-3 bg-light video-container">
+                <video class="" id="preview"></video>
+            </div>
+            <div class="cameras btn-group">
+                <!-- <button class="btn btn-primary start">Start</button> -->
+                <button class="btn btn-danger stop">Stop</button>
+                <button class="btn btn-dark mirror">Mirror</button>
+            </div>
+        </div>
     </div>
-    <button class="btn btn-danger stop">Stop</button>
+</div>
+<style>
+    .video-container {
+        height: 300px;
+        width: 300px;
+    }
 
-    <button class="btn btn-dark mirror">Mirror</button>
+    .video-container video {
+        width: 100%;
+        height: auto;
+    }
+</style>
 
-    <script type="text/javascript">
-        let scanner = new Instascan.Scanner({
-            video: document.getElementById('preview'),
-        });
-        scanner.addListener('scan', function(content) {
-            alert(content);
-        });
-        Instascan.Camera.getCameras().then(function(cameras) {
-            if (cameras.length > 0) {
-                for (i = 0; i < cameras.length; i++) {
-                    console.log(cameras[i]);
-                    $('.cameras').append('<button class="btn btn-primary start" data-id="' + i + '">Camera ' + (i + 1) + '</button>');
-                    $('.start[data-id="' + i + '"]').data('id', cameras[i]);
-                }
-            } else {
-                console.error('No cameras found.');
+
+
+<script type="text/javascript">
+    let scanner = new Instascan.Scanner({
+        video: document.getElementById('preview'),
+    });
+    scanner.addListener('scan', function(content) {
+        alert(content);
+    });
+    Instascan.Camera.getCameras().then(function(cameras) {
+        if (cameras.length > 0) {
+            for (i = 0; i < cameras.length; i++) {
+                console.log(cameras[i]);
+                $('.cameras').prepend('<button class="btn btn-primary start" data-id="' + i + '">' + cameras[i].name + '</button>');
+                $('.start[data-id="' + i + '"]').data('id', cameras[i]);
             }
-        }).catch(function(e) {
-            console.error(e);
-        });
-        $(document).on('click', '.start', function() {
-            console.log($(this).data('id'));
-            scanner.start($(this).data('id'));
-        })
+        } else {
+            console.error('No cameras found.');
+        }
+    }).catch(function(e) {
+        console.error(e);
+    });
+    $(document).on('click', '.start', function() {
+        console.log($(this).data('id'));
+        scanner.start($(this).data('id'));
+    })
 
-        $('.stop').click(function() {
-            scanner.stop();
-        })
-        $('.mirror').click(function() {
-            if (scanner.mirror) {
-                scanner.mirror = false;
-            } else {
-                scanner.mirror = true;
-            }
-        })
-    </script>
-
-</body>
-
-</html>
+    $('.stop').click(function() {
+        scanner.stop();
+    })
+    $('.mirror').click(function() {
+        if (scanner.mirror) {
+            scanner.mirror = false;
+        } else {
+            scanner.mirror = true;
+        }
+    })
+</script>
